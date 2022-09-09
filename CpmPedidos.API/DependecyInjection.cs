e13ddd22808106832c4dafe6f28641c0d68b20e1
rafaelrestolho12 <rafaelrestolho12@gmail.com>
2022-09-08 21:17:50 -0300

    Mapeamento

---
+++ CpmPedidos.API/DependecyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CpmPedidos.API
+{
+    public class DependecyInjection
+    {
+        public static void Register(IServiceCollection serviceProvider)
+        {
+            RepositoryDependence(serviceProvider);
+        }
+
+        private static void RepositoryDependence(IServiceCollection serviceProvider)
+        {
+            //serviceProvider.AddScoped<IProdutoRepository, ProdutoRepository>();
+            //serviceProvider.AddScoped<IPedidoRepository, PedidoRepository>();
+            //serviceProvider.AddScoped<ICidadeRepository, CidadeRepository>();
+        }
+    }
+}
---
+++ CpmPedidos.Domain/Entities/Cidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpmPedidos.Domain
+{
+    public class Cidade : BaseDomain, IExibivel
+    {
+        public string Nome { get; set; }
+
+        public string Uf { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
